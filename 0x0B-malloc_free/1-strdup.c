5c8d02a829c026409d0ff0291d84d81fde005fec
tunnerfrank <tunnerfrank116@gmail.com>
2023-09-05 19:08:18 +0300

    string duplication

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strdup - returns the string given as parameter
+ * Return: malloc pointer
+ * @str: the string given as parameter which will be duplicated
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int length;
+	int i;
+
+	length = strlen(str);
+	s = malloc((length + 1) * sizeof(char));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	length = strlen(str);
+	s = malloc((length + 1) * sizeof(char));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < length; i++)
+		{
+			s[i] = str[i];
+		}
+		s[i] = '\0';
+	}
+	return (s);
+}
